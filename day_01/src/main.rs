b5dbaeb5b05beda18f719cd2a9f9c3d63be6c815
Zephyr Mann <zephmann@gmail.com>
2022-12-05 23:42:45 -0500

    Finish day 01.

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,3 +1,37 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let file_path = &args[1];
+    let num_to_add: usize = args[2].parse().expect("Couldn't parse num_to_add arg");
+
+    println!("File path: {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    // create a list of the sums, then 
+    let mut sums = Vec::new();
+    let mut cur_sum = 0;
+    for line in contents.lines() {
+        if line == "" {
+            println!("Pushing: {}", cur_sum);
+            sums.push(cur_sum);
+            cur_sum = 0;
+        }
+        else {
+            let x: u32 = line.parse().expect("Couldn't parse line");
+            cur_sum += x;
+        }
+    }
+    sums.sort();
+    sums.reverse();
+
+    println!("Adding: {:?}", &sums[..num_to_add]);
+
+    let sum: u32 = sums[..num_to_add].iter().sum();
+
+    println!("Sum: {}", sum);
 }
